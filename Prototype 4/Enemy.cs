8c5b0f12ff982bd9fdb55095842ffffbc0635510
Endxw <167477332+Endxw@users.noreply.github.com>
2025-09-05 17:09:46 +0100

    Update Enemy.cs

--- Prototype 4/Enemy.cs
+++ Prototype 4/Enemy.cs
@@ -2,20 +2,27 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float speed;
+    private float speed = 1.0f;
     private Rigidbody enemyRb;
     private GameObject player;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     void Start()
     {
+        // Get the Rigidbody component attached to the enemy and find the player object in the scene
         enemyRb = GetComponent<Rigidbody>();
         player = GameObject.Find("Player");
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
+        // Calculate the direction towards the player and move the enemy in that direction
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
         enemyRb.AddForce(lookDirection * speed);
+
+        if (transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
     }
 }
