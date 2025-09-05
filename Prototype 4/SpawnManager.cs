68fa5e942f6972f17ea5f305a4ac052491518ac8
Endxw <167477332+Endxw@users.noreply.github.com>
2025-09-05 17:09:11 +0100

    Update SpawnManager.cs

--- Prototype 4/SpawnManager.cs
+++ Prototype 4/SpawnManager.cs
@@ -3,15 +3,52 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject powerupPrefab;
+    private float spawnRange= 9.0f;
+    private int enemyCount;
+    private int waveNumber = 1;
+
+
+
     void Start()
     {
-        Instantiate(enemyPrefab, new Vector3(0, 0, 6), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPos(), powerupPrefab.transform.rotation);
     }
 
-    // Update is called once per frame
+        
     void Update()
     {
-        
+        // Check the number of enemies currently in the scene and spawn a new wave if there are none left
+        enemyCount = FindObjectsByType<Enemy>(FindObjectsSortMode.None).Length;
+
+        if (enemyCount == 0)
+        {
+            // Increment the wave number and spawn a new wave of enemies along with a powerup
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPos(), powerupPrefab.transform.rotation);
+        }
+    }
+
+    private void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        // Spawn 3 enemies at random positions within the spawn range
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPos(), enemyPrefab.transform.rotation);
+        }
+    }
+
+    private Vector3 GenerateSpawnPos()
+    {
+        // Generate a random position within the defined spawn range
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+
+        // Create a Vector3 for the spawn position, keeping the y-coordinate at 0
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+
+        return randomPos; // Return the generated random position
     }
 }
